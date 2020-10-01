cc44cd16d6a6e443ac407e29ac80758e483f9d5e
lotsofluck <anitabangali@yahoo.com>
2020-09-30 19:05:14 -0500

    Update scripts.js
    cart fully functional

--- Resources/js/scripts.js
+++ Resources/js/scripts.js
@@ -34,12 +34,32 @@ all_items_button.forEach(et => Element.addEventListner('click', () =>{
   showItems()
 }))
 
-const cart = [ ]
+const cart = []
+itemList.onchange = function(e) {
+  if (e.target && e.target.classList.contains('update')) {
+    const name = e.target.dateset.name
+    const qty = parseInt(e.target.value)
+    updateCart(name, qty)
+  }
+}
+itemList.onclick = function() {
+ if (e.target && e.target.classList.contains('remove')) {
+  const name = e.target.dataset.name  
+  removeItem(name)
+ } else if (e.target && e.target.classList.contains('add-one')){
+  const name = e.target.dataset.name
+  addItem(name)
+ } else if (e.target && e.target.classList.contains('remove-one')) {
+  const name = e.target.dataset.name
+  removeItem(name, 1)
+ }
+}
 // Add item function
 function addItem(name,price) {
   for (let i =0; i < cart.length; i +=1) {
     if (cart[i].name === name) {
       cart[i].qty += 1
+      showItems()
       return
     }
   }
@@ -62,7 +82,13 @@ function showItems() {
 
      const { name, price, qty} = cart[i]
      
-     itemStr += `<li> ${name} $${price} x ${qty} = ${qty * price}</li>`
+     itemStr += `<li> 
+      ${name} $${price} x ${qty} = ${qty * price} 
+      < button class="remove" data-name=${name}""> Remove</button>
+      < button class="add-one" data-name=${name}""> Remove</button>
+      < button class="remove-one" data-name=${name}""> Remove</button>
+      < input class="update" type="number" min="0" data-name="${name}">
+     </li>`
    }
    itemList.innerHTML = itemStr
    
@@ -96,7 +122,22 @@ function removeItem(name, qty = 0) {
       if (cart[i].qty < 1  || qty === 0) {
         cart.splice(i, 1)
       }
+      showItems()
       return 
+    }
+  }
+}
+
+function updateCart(name,qty) {
+  for (let i = 0; i < cart.length; i += 1) {
+    if (cart[i].name === name) {
+      if (qty < 1) {
+        removeItem(name)
+        return
+      }
+      cart[i].qty = qty
+      showItems()
+      return
     }
   }
 }
